524e9cc5c5e7d4e1b9fbffbed0dbadf103d5a4e2
ducntdev94 <ducntdev94@gmail.com>
2026-01-03 20:46:48 +0700

    + changes

--- config/site.ts
+++ config/site.ts
@@ -30,7 +30,7 @@ export type SiteConfig = {
 export const siteConfig: SiteConfig = {
     name: "Avocado Spa & Skin Care",
     h2: "Spa nặn mụn & điều trị da chuẩn y khoa tại Đồng Xoài",
-    description: "“Chuyên nặn mụn ẩn, mụn viêm – không thâm sẹo. Home spa uy tín tại Đồng Xoài, nhận khách tại nhà.”",
+    description: "“Chuyên nặn mụn, điều trị mụn, phục hồi da. Home spa uy tín.”",
     theme: "spa",
     heroImage: "https://images.unsplash.com/photo-1540555700478-4be289fbecef?auto=format&fit=crop&q=80&w=2600", // Spa/Massage image
     mainNav: [
@@ -60,11 +60,11 @@ export const siteConfig: SiteConfig = {
         contact: true,
     },
     contact: {
-        phone: "096 743 XXXX",
+        phone: "0394675965",
         address: "Avocado Skincare 60 Phan Huy Ích, Tân Phú, Đồng Xoài, Bình Phước", // Updated example address
         email: "booking@avocado.com",
-        zalo: "https://zalo.me/096743XXXX",
-        facebook: "https://www.facebook.com/profile.php?id=61563770278945",
+        zalo: "https://zalo.me/0394675965",
+        facebook: "https://www.facebook.com/avocadoskincaredongxoai/",
         mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1789.1154824734938!2d106.89874869325409!3d11.546308630669976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317361002c88967d%3A0xe0b4ba9d94f50a82!2sAvocado%20Skincare!5e1!3m2!1svi!2s!4v1767344531037!5m2!1svi!2s"
     }
 }
